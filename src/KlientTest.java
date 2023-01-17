76d79a3a28def5e7668f8511177e4cd80fd45517
Franciszek Wojnowicz <62144431+franciszekwojnowicz@users.noreply.github.com>
2023-01-17 02:32:39 +0100

    Update KlientTest.java

--- src/KlientTest.java
+++ src/KlientTest.java
@@ -2,8 +2,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Vector;
+import org.junit.experimental.categories.Category;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+//@RunWith(Parameterized.class)
 public class KlientTest {
     private Klient klient;
 
@@ -11,15 +18,38 @@ public class KlientTest {
     public void setUp(){
       klient= new Klient("J","W",007,997,"woj@wp.pl");
     }
+    final Transakcja transakcja = new Transakcja(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 1), 10.0f,
+            new Vector(List.of(new Produkt("nazwa", 1.0f, 0, 'a'))), 0.0f);
 
+
+    @Category({KlientTest.class})
     @Test
     public void TestgetEmail() {
         Assert.assertEquals("woj@wp.pl", klient.getEmail());
     }
 
+    @Category({KlientTest.class})
     @Test
     public void TestsetEmail() {
         klient.setEmail("nowy@gmail.com");
         Assert.assertEquals("nowy@gmail.com",klient.getEmail());
     }
+
+
+    //dziesiejsza data przed rezerwacją
+    @Category({KlientTest.class})
+    @Test
+    public void testAnulujRezerwacje() {
+        final boolean result = klient.anulujRezerwacje(transakcja, LocalDate.of(2019, 1, 1));
+        assertTrue(result);
+    }
+    //dziesiejsza data po rezerwacji
+    @Category({KlientTest.class})
+    @Test
+    public void testAnulujRezerwacje1() {
+        final boolean result = klient.anulujRezerwacje(transakcja, LocalDate.of(2020, 1, 1));
+        assertFalse(result);
+    }
+
+
 }
